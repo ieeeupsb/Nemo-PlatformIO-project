71a15bf3d6643a0f7514bbf7e727d328f2791264
ManelMCCS <manuelmagalhaes7@gmail.com>
2022-04-28 23:20:18 +0100

    line case

--- lib/line/line.h
+++ lib/line/line.h
@@ -4,12 +4,14 @@
 
 // L4 will be the oposite of the others, need to negate
 
-#define CORRECT_TO_RIGHT 12 // NEED TO TURN RIGHT 0010  //0011
-#define CORRECT_TO_LEFT 136 // NEED TO TURN LEFT 0100   //0101
+#define CORRECT_TO_RIGHT 24 // NEED TO TURN RIGHT 0010  //0011
+#define CORRECT_TO_RIGHT 24 // NEED TO TURN RIGHT 0010  //0011
+#define SHARP_LEFT 12       // NEED TO TURN LEFT 0100   //0101
+#define SHARP_RIGHT 0       // NEED TO TURN LEFT 0100   //0101
 #define RIGHT_CURVE 156  // 0111                     //0110  //estavam trocados
-#define LEFT_CURVE 132   // 1110                     //1111  //estavam trocados
+#define LEFT_CURVE 144   // 1110                     //1111  //estavam trocados
 #define INTERCEPTION 148 // 1111                     //1110
-#define LINE 140         // 0110                     //0111
+#define LINE 152         // 0110                     //0111
 #define FREE 8           // 0000                     //0001
 
 #define line_state(__LINE_SENSOR__) (digitalRead(__LINE_SENSOR__))
