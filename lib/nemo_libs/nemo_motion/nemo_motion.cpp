ddaeea30dacd667f9b789649d476cb840b3821ce
ManelMCCS <up201806391@fe.up.pt>
2022-09-06 15:31:42 +0100

    fix error in motion.fresh

---
+++ lib/nemo_libs/nemo_motion/nemo_motion.cpp
@@ -0,0 +1,25 @@
+#include "nemo_motion.h"
+
+#define LEFT_MOTOR_CHANNEL 0
+#define RIGHT_MOTOR_CHANNEL 1
+
+#define MASTER_MOTOR left_motor
+
+void motion::setup() {
+    ESP32Encoder::useInternalWeakPullResistors = UP;
+    left_motor.setup(DC_MOTOR_1L, DC_MOTOR_2L, ENABLE_L, LEFT_MOTOR_CHANNEL,
+                     ENC1_L, ENC2_L);
+    right_motor.setup(DC_MOTOR_1R, DC_MOTOR_2R, ENABLE_R, RIGHT_MOTOR_CHANNEL,
+                      ENC1_R, ENC2_R);
+}
+
+void motion::refresh() {
+    if (max_ticks == MASTER_MOTOR.encoder.getCount())
+        stop();
+    linear_speed = MASTER_MOTOR.get_speed();
+}
+
+void motion::stop() {
+    left_motor.stop();
+    right_motor.stop();
+}
