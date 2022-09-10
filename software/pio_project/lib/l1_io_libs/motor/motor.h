0548e4987b6f60f3605d99a66fbcd4620c3ec03d
ManelMCCS <up201806391@fe.up.pt>
2022-09-10 14:03:49 +0100

    motion: motor class minor fixes and documentation

--- software/pio_project/lib/l1_io_libs/motor/motor.h
+++ software/pio_project/lib/l1_io_libs/motor/motor.h
@@ -2,8 +2,8 @@
 #pragma once
 /**
  * @file motor.h
- * @author manel
- * @brief
+ * @author Manuel Cerqueira da Silva and Vinícius Pirassoli.
+ * @brief Library to control Nemo DC Motors.
  * @version 0.1
  * @date 2022-06-06
  *
@@ -19,50 +19,109 @@
 #define MAX_PWM 255
 #endif
 
+/**
+ * @brief Motor return type to handle errors using the Motor class.
+ *
+ */
 typedef enum { MOTOR_ERROR = 0,
                MOTOR_SUCCESS } motor_return_t;
 
 /**
- * @brief Motor direction flags
+ * @brief Motor direction flags type.
  *
  */
 typedef enum { ROTATION_NOT_DEFINED = 0,
                CLOCKWISE,
                ANTI_CLOCKWISE } motor_rotation_dir_t;
 
+// WARN
 /**
- * @brief Motor is a class that contains motor function data and controls the DC
- * motor through the signals from the encoders.
+ * @brief PWM variable type.
+ *
+ * PWM range is 0 to 255, store in a 8 bit unsigned integer.
+ *
+ */
+typedef uint8_t pwm_t;
+
+// FIXME
+/**
+ * @brief Motor is a class used for controling a DC Motor.
+ *
+ * This class stores the motors pins and the motor **não sei que termo usar, help** via PWM (0-255).
+ * Each motor have 3 pins:
+ *    The DC motor enable pin to control the motor the **o mesmmo problema de cima**
+ *    The 2 DC motor terminal pins to control and current flow in the motor and consequently the rotation direction.
  *
  */
 class Motor {
   public:
     void setup(uint8_t dc_motor_1_pin, uint8_t dc_motor_2_pin,
                uint8_t enable_pin, uint8_t pwm_channel);
+
     /**
-     * @brief Stops the motor (sets pwm to 0)
+     * @brief Stops the motor (sets PWM to 0)
      *
      */
     void stop();
 
-    uint32_t pwm_val = 0;
-
+    /**
+     * @brief Set the motor rotation direction. The allowed values are CLOCKWISE and ANTI_CLOCKWISE.
+     *
+     * @param motor_rotation_dir
+     */
     void set_direction(motor_rotation_dir_t motor_rotation_dir);
-    int pwm_offset(int pwm_dif);
-    motor_return_t set_pwm(unsigned int _pwmVal);
 
-    int get_pwm();
+    /**
+     * @brief Set the a PWM given by the user and stores the value.
+     *
+     * If the PWM given is not in the valid range this method returns MOTOR_ERROR. Otherwise returns MOTOR_SUCCESS.
+     *
+     * @param pwm_val
+     * @return motor_return_t
+     */
+    motor_return_t set_pwm(pwm_t pwm_val);
+
+    /**
+     * @brief Increase or decrease the PWM value by the offset value.
+     *
+     * @param offset_val
+     * @return motor_return_t
+     */
+    motor_return_t pwm_offset(pwm_t offset_val);
+
+    /**
+     * @brief Get the PWM value from the motor.
+     *
+     * @return pwm_t
+     */
+    pwm_t get_pwm();
 
   private:
+    /**
+     * @brief Last PWM value given to the motor enable analog pin.
+     *
+     * This is variable is set to private to prevent changing only the variable and not the real Motor PWM.
+     * For change motor PWM value use pwm_offset and the set_pwm  Motor's methods.
+     * For check the current pwm value outside of this library you should use the get_pwm Motor's method.
+     *
+     */
+    pwm_t current_pwm = 0;
 #if ESP32
+    // For using the analog pins with the ESP32 it is necessary to configure a PWM channel.
+
     uint8_t pwm_channel;
     double pwm_frequency = 5000;
     uint8_t resolution_bits = 8;
 #endif
-
+    /**
+     * @brief DC motor digital pin number 1 used to control the current flow inside the motor.
+     *
+     */
     uint8_t dc_motor_1;
-    uint8_t dc_motor_2;
 
-    float previous_tick_number = 0;
-    float previous_time = 0;
+    /**
+     * @brief DC motor digital pin number 1 used to control the current flow inside the motor.
+     *
+     */
+    uint8_t dc_motor_2;
 };
