fcf5f620d3a0f7d885757f50f56c6a0d2079921c
ManelMCCS <up201806391@fe.up.pt>
2023-01-30 20:07:43 +0000

    pi for right motor

--- software/pio_project/src/main.cpp
+++ software/pio_project/src/main.cpp
@@ -4,7 +4,6 @@
 #include "Arduino.h"
 #include "PID_v1.h"
 #include "RPi_Pico_TimerInterrupt.h"
-#include "encoder_reader.h"
 #include "micro_controller_unit.h"
 #include "pidautotuner.h"
 
@@ -13,51 +12,80 @@ RPI_PICO_Timer ITimer0(0);
 
 MicroControllerUnit &mcu = MicroControllerUnit::getInstance();
 
-// // EncoderReader left_encoder_reader(ENC_C1_PIN_R, ENC_C2_PIN_R);
-EncoderReader right_encoder_reader(ENC_C1_PIN_R, ENC_C2_PIN_R);
+DriverController left_driver_controller(DRIVER_ENABLE_PIN_L, DRIVER_IN_A_PIN_L, DRIVER_IN_B_PIN_L);
 DriverController right_driver_controller(DRIVER_ENABLE_PIN_R, DRIVER_IN_A_PIN_R, DRIVER_IN_B_PIN_R);
 
-double Setpoint, Input, Output;
+double target_speed_left, rpm_last_read_left_pid_input, left_pwm_output;
+double target_speed_right, rpm_last_read_right_pid_input, right_pwm_output;
 
-double Kp = 0.248 * 1, Ki = 0.000, Kd = 0;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+double Kp = 1 * 0.7, Ki = 1.4, Kd = 0;
+PID leftWheelPID(&rpm_last_read_left_pid_input, &left_pwm_output, &target_speed_left, Kp, Ki, Kd, DIRECT);
+PID rightWheelPID(&rpm_last_read_right_pid_input, &right_pwm_output, &target_speed_right, Kp, Ki, Kd, DIRECT);
 
-volatile int speed_counter_ = 0;
-volatile float speed;
+volatile int encoder_counter_left = 0;
+volatile int encoder_counter_right = 0;
 
 bool TimerHandler0(struct repeating_timer *t) {
-    right_encoder_reader.updateSpeed(TIMER0_INTERVAL_MS, speed_counter_);
-    speed = (float)speed_counter_ / (float)TIMER0_INTERVAL_MS;
-    speed = speed * 100;
-    speed_counter_ = 0;
+
+    float right_speed = (float)encoder_counter_right / (float)TIMER0_INTERVAL_MS;
+    float left_speed = (float)encoder_counter_left / (float)TIMER0_INTERVAL_MS;
+
+    rpm_last_read_right_pid_input = right_speed * 100;
+    rpm_last_read_left_pid_input = left_speed * 100;
+
+    encoder_counter_left = 0;
+    encoder_counter_right = 0;
+
+    leftWheelPID.Compute();
+    rightWheelPID.Compute();
+
+    left_driver_controller.setPwm(left_pwm_output);
+    right_driver_controller.setPwm(right_pwm_output);
 
     return true;
 }
 
-void updateCount() {
+void updateLeftCount() {
+    int a_state = digitalRead(ENC_C1_PIN_L);
+    int b_state = digitalRead(ENC_C2_PIN_L);
+
+    if (a_state == b_state) {
+        encoder_counter_left++;
+    } else {
+        encoder_counter_left--;
+    }
+}
+
+void updateRightCount() {
     int a_state = digitalRead(ENC_C1_PIN_R);
     int b_state = digitalRead(ENC_C2_PIN_R);
 
     if (a_state == b_state) {
-        speed_counter_++;
+        encoder_counter_right++;
     } else {
-        speed_counter_--;
+        encoder_counter_right--;
     }
 }
 
 void setup() {
     Serial.begin(9600);
 
-    pinMode(ENC_C1_PIN_R, INPUT);
-    pinMode(ENC_C2_PIN_R, INPUT);
-    attachInterrupt(digitalPinToInterrupt(ENC_C1_PIN_R), updateCount, CHANGE);
+    pinMode(ENC_C1_PIN_R, rpm_last_read_left_pid_input);
+    pinMode(ENC_C2_PIN_R, rpm_last_read_left_pid_input);
+
+    attachInterrupt(digitalPinToInterrupt(ENC_C1_PIN_L), updateLeftCount, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(ENC_C1_PIN_R), updateRightCount, CHANGE);
     // right_driver_controller.setPwm(70);
+    // left_driver_controller.setDirection(motor_rotation_dir_t::ANTI_CLOCKWISE);
     right_driver_controller.setDirection(motor_rotation_dir_t::CLOCKWISE);
+
     ITimer0.attachInterruptInterval(TIMER0_INTERVAL_MS * 1000, TimerHandler0);
-    Setpoint = 300;
+    target_speed_left = 0;
+    target_speed_right = 100;
 
     // turn the PID on
-    myPID.SetMode(AUTOMATIC);
+    leftWheelPID.SetMode(AUTOMATIC);
+    rightWheelPID.SetMode(AUTOMATIC);
 }
 
 #define TIME_INTERVAL 1000
@@ -65,42 +93,22 @@ void setup() {
 int speed_control_state = 0;
 
 void loop() {
+
+    Serial.print("rpm left: ");
+    Serial.println(rpm_last_read_left_pid_input);
+    Serial.print("rpm right: ");
+    Serial.println(rpm_last_read_right_pid_input);
+
     switch (speed_control_state) {
-    case 0:
-        Input = right_encoder_reader.getRpm();
-        Serial.print("rpm: ");
-        Serial.println(speed);
+    case 0: // from small loop for PID
+
         speed_control_state = 1;
         break;
-    case 1:
-        myPID.Compute();
-        Serial.print("PWM: ");
-        Serial.println(Output);
-
-        right_driver_controller.setPwm(Output);
-        speed_control_state = 0;
+    case 1: // get target
+        // gets target from User
+        // sets speed_control_state to 0
+        break;
     default:
         break;
     }
 }
-
-// void setup() {
-//     Serial.begin(115200);
-//     pinMode(ENC_C1_PIN_R, INPUT);
-//     pinMode(ENC_C2_PIN_R, INPUT);
-//     pinMode(ENC_C1_PIN_R, INPUT);
-//     pinMode(ENC_C2_PIN_R, INPUT);
-// }
-
-// void loop() {
-//     Serial.print("ENC_C1_PIN_R:");
-//     Serial.println(digitalRead(ENC_C1_PIN_R));
-//     Serial.print("ENC_C2_PIN_R:");
-//     Serial.println(digitalRead(ENC_C2_PIN_R));
-//     Serial.print("ENC_C1_PIN_R:");
-//     Serial.println(digitalRead(ENC_C1_PIN_R));
-//     Serial.print("ENC_C2_PIN_R:");
-//     Serial.println(digitalRead(ENC_C2_PIN_R));
-//     Serial.println("\n\n\n\n\n\n");
-//     delay(1000);
-// }
