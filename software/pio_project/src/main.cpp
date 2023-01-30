8eaf89749c2c95a6634924b699e0be666780ae6e
ManelMCCS <up201806391@fe.up.pt>
2023-01-30 18:32:16 +0000

    pid k is working

--- software/pio_project/src/main.cpp
+++ software/pio_project/src/main.cpp
@@ -19,8 +19,8 @@ DriverController right_driver_controller(DRIVER_ENABLE_PIN_R, DRIVER_IN_A_PIN_R,
 
 double Setpoint, Input, Output;
 
-// double Kp = 0.7, Ki = 0, Kd = 0;
-// PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+double Kp = 0.248 * 1, Ki = 0.000, Kd = 0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 volatile int speed_counter_ = 0;
 volatile float speed;
@@ -28,6 +28,7 @@ volatile float speed;
 bool TimerHandler0(struct repeating_timer *t) {
     right_encoder_reader.updateSpeed(TIMER0_INTERVAL_MS, speed_counter_);
     speed = (float)speed_counter_ / (float)TIMER0_INTERVAL_MS;
+    speed = speed * 100;
     speed_counter_ = 0;
 
     return true;
@@ -50,13 +51,13 @@ void setup() {
     pinMode(ENC_C1_PIN_R, INPUT);
     pinMode(ENC_C2_PIN_R, INPUT);
     attachInterrupt(digitalPinToInterrupt(ENC_C1_PIN_R), updateCount, CHANGE);
-    right_driver_controller.setPwm(70);
+    // right_driver_controller.setPwm(70);
     right_driver_controller.setDirection(motor_rotation_dir_t::CLOCKWISE);
     ITimer0.attachInterruptInterval(TIMER0_INTERVAL_MS * 1000, TimerHandler0);
-    Setpoint = 2;
+    Setpoint = 300;
 
     // turn the PID on
-    // myPID.SetMode(AUTOMATIC);
+    myPID.SetMode(AUTOMATIC);
 }
 
 #define TIME_INTERVAL 1000
@@ -67,16 +68,16 @@ void loop() {
     switch (speed_control_state) {
     case 0:
         Input = right_encoder_reader.getRpm();
-        Serial.println("Input rpm: ");
+        Serial.print("rpm: ");
         Serial.println(speed);
-        // speed_control_state = 1;
+        speed_control_state = 1;
         break;
     case 1:
-        // myPID.Compute();
-        // Serial.println("Output: ");
-        // Serial.println(Output);
+        myPID.Compute();
+        Serial.print("PWM: ");
+        Serial.println(Output);
 
-        // right_driver_controller.setPwm(Output);
+        right_driver_controller.setPwm(Output);
         speed_control_state = 0;
     default:
         break;
