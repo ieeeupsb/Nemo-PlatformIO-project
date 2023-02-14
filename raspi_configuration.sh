80333081fcc6b3fc9b1fa9a94ec5459b0780e8e2
Manuel Cerqueira da Silva <67638640+ManelMCCS@users.noreply.github.com>
2023-02-14 21:21:34 +0000

    Update raspi_configuration.sh

--- raspi_configuration.sh
+++ raspi_configuration.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
+git clone https://github.com/ieeeupsb/Nemo.git
+
 #Numpy
 sudo apt install python3-numpy
 
 # OpenCV
-sudo apt-get install python-opencv
+dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 python3-pyqt5 python3-dev -y
+pip install -U numpy
+pip install opencv-python==4.5.3.56
+
+#matplotlib
+sudo apt install python3-matplotlib python3-tk
+
+# install pandas
+pip install pandas
 
